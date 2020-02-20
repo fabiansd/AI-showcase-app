752305a18ef2d6e6fb38e74554ddfc4fd4aa331f
Fabian Sødal Dietrichson <fabian.sodal.dietrichson@vegvesen.no>
2020-02-20 22:44:12 +0100

    Implemented flexbox for dividing main body

--- application-klient/src/components/Body.tsx
+++ application-klient/src/components/Body.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Grid, Header, Icon, Image } from 'semantic-ui-react';
+import { Divider, Grid, Header, Icon, Image } from 'semantic-ui-react';
 import '../style/components.scss';
 import Workcard from './Workcard';
 
 class Body extends React.Component {
     render() {
-        return <section>
+        return <div className="bodycard shadoweffect">
             <Grid divided='vertically'>
-                <Grid.Row columns={2} padded>
+                <Grid.Row columns={2}>
                     <Grid.Column width={8} textAlign="left">
                         <p>
                             THIS IS MEEEEE
@@ -16,28 +16,29 @@ class Body extends React.Component {
                     <Grid.Column width={4} floated="right">
                         <Image src='../../public/images/fabian.s.dietrichson.jpg' />
                     </Grid.Column>
+                <div className="ui divider"></div>
                 </Grid.Row>
 
-                <Grid.Row columns={2} padded={2}>
-                    <Grid.Column>
+
+                <div className="flex-container">
+                    <div className="flex-item-1">
                         <Header as='h2'>
                             <Icon name="university" />
                             <Header.Content>Uptime Guarantee</Header.Content>
                         </Header>
                         <Workcard/>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <Header as='h2'>
-                            <Icon name="university" />
-                            <Header.Content>Uptime Guarantee</Header.Content>
+                    </div>
+                    <div className="flex-item-2">
+                        <Header as='h3'>*/}
+                            <Header.Content>So What!!</Header.Content>
+                            <p>
+                                Amy is a violinist with 2 years experience in the wedding industry. She enjoys the outdoors and currently resides in upstate New York.Amy is a violinist with 2 years experience in the wedding industry. She enjoys the outdoors and currently resides in upstate New York.Amy is a violinist with 2 years experience in the wedding industry. She enjoys the outdoors and currently resides in upstate New York.Amy is a violinist with 2 years experience in the wedding industry. She enjoys the outdoors and currently resides in upstate New York.Amy is a violinist with 2 years experience in the wedding industry. She enjoys the outdoors and currently resides in upstate New York.
+                            </p>
                         </Header>
-                        <div className="m-2">
-                            <Workcard/>
-                        </div>
-                    </Grid.Column>
-                </Grid.Row>
+                    </div>
+                </div>
             </Grid>
-        </section>;
+        </div>;
     }
 }
 
--- application-klient/src/components/Workcard.tsx
+++ application-klient/src/components/Workcard.tsx
@@ -6,8 +6,8 @@ class Workcard extends React.Component {
 
     render() {
         return (
-            <Card>
-                <Card.Content header='About Amy' />
+            <Card fluid textAlign="left">
+                <Card.Content header='About Amy'/>
                 <Card.Content description={"Amy is a violinist with 2 years experience in the wedding industry. She enjoys the outdoors and currently resides in upstate New York."} />
                 <Card.Content extra>
                     <Icon name='user' />See more
